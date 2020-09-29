c8f3d883834d02a49623fab24cdcf1a3146b1da6
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2020-09-29 17:45:11 +0200

    Correct font preload url

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,7 @@ class CustomDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="I'm Jonathan, a front-end developer from Gothenburg" />
 
-          <link rel="preload" href="/fonts/patua-one-v10-regular.woff2" as="font" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/source-sans-pro-v13-light.woff2" as="font" crossOrigin="anonymous" />
 
           {styleTags}
         </Head>
