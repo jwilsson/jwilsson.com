fce6d6f1c98e086511eb58b65db9945380c99507
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2020-10-20 17:46:43 +0200

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -39,6 +39,7 @@ class CustomDocument extends Document {
           <meta name="description" content="I'm Jonathan, a front-end developer from Gothenburg" />
 
           <link rel="preload" href="/fonts/source-sans-pro-v13-light.woff2" as="font" crossOrigin="anonymous" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
           {styleTags}
         </Head>
