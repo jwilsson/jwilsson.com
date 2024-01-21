77c769375ef8591e334db522a2d9f3ae7b80c742
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2024-01-21 13:02:57 +0100

    Setup layout metadata correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next';
 import { FC, PropsWithChildren } from 'react';
 import './globals.css';
+
+export const metadata: Metadata = {
+    description: "I'm Jonathan, a front-end developer from Gothenburg",
+    title: 'Jonathan Wilsson - Front-end developer',
+};
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => (
     <html lang="en" className="antialiased bg-primary min-h-full relative">
